e2394826b7ac17563a0c9a0af29fdebee0fa9dd4
TechUser <repo@techuser.in>
2018-01-09 13:46:45 +0530

    JavaScript

---
+++ js/test.js
@@ -0,0 +1,21 @@
+function myHide() {
+document.getElementById('hide').style.display='none';
+
+}
+
+function myShow() {
+  document.getElementById('hide').style.display='block';
+}
+
+
+//Multiple statements calling
+function myName() {
+  document.getElementById('name2').innerHTML = "Hello, Ganesh";
+  document.getElementById('name3').innerHTML = "Vadla, How are you ?";
+}
+
+
+/*
+Multi line comments 
+
+*/
